70481a16e0af782cb3b108497e77e7fe05cbff4d
Yushi Takahashi <y.takahashi.la89@gmail.com>
2021-09-21 08:24:58 +0900

    いったんコミット

--- src/views/ActionCard.vue
+++ src/views/ActionCard.vue
@@ -1,9 +1,114 @@
 <template>
+  <ion-card v-for="(actionImages, dirName) in dirImages" :key="dirName">
+    <div>ION-CARD</div>
+    <ion-grid>
+      <ion-row>
+        <ion-col
+          v-for="(actionImages, platform) in actionImages"
+          :key="platform"
+        >
+          <ion-grid>
+            <ion-row>
+              <ion-col>
+                <div v-for="(image, name) in actionImages.old" :key="name">
+                  old_{{ name }}
+                </div>
+                <!-- <img :src="image.path" /> -->
+              </ion-col>
+              <ion-col>
+                <div v-for="(image, name) in actionImages.old" :key="name">
+                  new_{{ name }}
+                </div>
+                <!-- <img :src="image.path" /> -->
+              </ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+  </ion-card>
+  <ion-card>
+    <ion-grid>
+      <ion-row class="ion-align-items-start">
+        <ion-col>
+          <div>1 of 4</div>
+        </ion-col>
+        <ion-col>
+          <div>2 of 4</div>
+        </ion-col>
+        <ion-col>
+          <div>3 of 4</div>
+        </ion-col>
+        <ion-col>
+          <div>
+            4 of 4 <br />
+            # <br />
+            # <br />
+            #
+          </div>
+        </ion-col>
+      </ion-row>
+
+      <ion-row class="ion-align-items-center">
+        <ion-col>
+          <div>1 of 4</div>
+        </ion-col>
+        <ion-col>
+          <div>2 of 4</div>
+        </ion-col>
+        <ion-col>
+          <div>3 of 4</div>
+        </ion-col>
+        <ion-col>
+          <div>
+            4 of 4 <br />
+            # <br />
+            # <br />
+            #
+          </div>
+        </ion-col>
+      </ion-row>
+
+      <ion-row class="ion-align-items-end">
+        <ion-col>
+          <div>1 of 4</div>
+        </ion-col>
+        <ion-col>
+          <div>2 of 4</div>
+        </ion-col>
+        <ion-col>
+          <div>3 of 4</div>
+        </ion-col>
+        <ion-col>
+          <div>
+            4 of 4 <br />
+            # <br />
+            # <br />
+            #
+          </div>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+  </ion-card>
   <ion-card>
     <ion-grid>
       <ion-row>
-        <ion-col v-for="(image, key) in images" :key="key">
-          <img :src="image.path" />
+        <ion-col class="ion-align-self-start">
+          <div>1 of 4</div>
+        </ion-col>
+        <ion-col class="ion-align-self-center">
+          <div>2 of 4</div>
+        </ion-col>
+        <ion-col class="ion-align-self-end">
+          <div>3 of 4</div>
+        </ion-col>
+        <ion-col>
+          <div>
+            4 of 4 <br />
+            # <br />
+            # <br />
+            #
+          </div>
         </ion-col>
       </ion-row>
     </ion-grid>
@@ -40,14 +145,42 @@ export default defineComponent({
     IonRow,
   },
   setup() {
-    const images = [
-      { path: "/images/Screenshot-001.png", name: "image1" },
-      { path: "/images/Screenshot-001.png", name: "image2" },
-      { path: "/images/Screenshot-001.png", name: "image3" },
-      { path: "/images/Screenshot-001.png", name: "image4" },
-    ];
-
-    return { images, warning };
+    const dirImages = {
+      dirName: "dir01",
+      dirImage: [
+        {
+          platform: "chrome",
+          old: [
+            { path: "/images/Screenshot-001.png", name: "image1" },
+            { path: "/images/Screenshot-001.png", name: "image2" },
+            { path: "/images/Screenshot-001.png", name: "image3" },
+            { path: "/images/Screenshot-001.png", name: "image4" },
+          ],
+          new: [
+            { path: "/images/Screenshot-001.png", name: "image1" },
+            { path: "/images/Screenshot-001.png", name: "image2" },
+            { path: "/images/Screenshot-001.png", name: "image3" },
+            { path: "/images/Screenshot-001.png", name: "image4" },
+          ],
+        },
+        {
+          platform: "IE",
+          old: [
+            { path: "/images/Screenshot-001.png", name: "image1" },
+            { path: "/images/Screenshot-001.png", name: "image2" },
+            { path: "/images/Screenshot-001.png", name: "image3" },
+            { path: "/images/Screenshot-001.png", name: "image4" },
+          ],
+          new: [
+            { path: "/images/Screenshot-001.png", name: "image1" },
+            { path: "/images/Screenshot-001.png", name: "image2" },
+            { path: "/images/Screenshot-001.png", name: "image3" },
+            { path: "/images/Screenshot-001.png", name: "image4" },
+          ],
+        },
+      ],
+    };
+    return { dirImages, warning };
   },
 });
 </script>
